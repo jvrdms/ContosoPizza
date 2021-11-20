c29e824eb32e140bc4c7d5f10ac50670e6f919ce
jvrdms <jvrdms@gmail.com>
2021-11-20 14:26:15 +0100

    net6.0 finished

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,61 @@
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaController : ControllerBase
+{
+  public PizzaController() { }
+
+  [HttpGet] // Get all action.
+  public ActionResult<List<Pizza>> GetAll() =>
+    PizzaService.GetAll();
+
+  [HttpGet("{id}")] // Get by Id action
+  public ActionResult<Pizza> Get(int id)
+  {
+    var pizza = PizzaService.Get(id);
+
+    if (pizza == null)
+      return NotFound();
+
+    return pizza;
+  }
+
+  [HttpPost] // POST action
+  public IActionResult Create(Pizza pizza)
+  {
+    PizzaService.Add(pizza);
+    return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+  }
+
+  [HttpPut("{id}")] // PUT action
+  public IActionResult Update(int id, Pizza pizza)
+  {
+    if (id != pizza.Id)
+      return BadRequest();
+
+    var existingPizza = PizzaService.Get(id);
+    if (existingPizza == null)
+      return NotFound();
+
+    PizzaService.Update(pizza);
+
+    return NoContent();
+  }
+
+  [HttpDelete("{id}")] // DELETE action
+  public IActionResult Delete(int id)
+  {
+    var pizza = PizzaService.Get(id);
+    if (pizza == null)
+      return NotFound();
+
+    PizzaService.Delete(id);
+
+    return NoContent();
+  }
+}
